e274489eca5f02b7a3381110746e97f34041ae17
GKunc <grzeg.kunc@gmail.com>
2024-06-16 19:46:07 +0200

    Translation with news

--- website/home/models.py
+++ website/home/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
-from wagtail.models import Page
-from wagtail.admin.panels import FieldPanel
-from wagtail.fields import RichTextField
+from wagtail.models import Page, Orderable
+
+from wagtail.admin.panels import (
+    MultiFieldPanel,
+    InlinePanel,
+    FieldPanel,
+    PageChooserPanel,
+    FieldRowPanel,
+)
+from wagtail.fields import (
+    RichTextField,
+)
+
+from django.db import models
+
+from django_extensions.db.fields import AutoSlugField
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from wagtail.snippets.models import register_snippet
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField,
+)
+
+
 
 class HomePage(Page):
-    templates = "templates/home/welcome_page.html"
+    templates = "templates/home/home_page.html"
 
     banner_title = models.CharField(max_length=100, blank=False, null=True)
     banner_subtitle = RichTextField(features=["bold", "italic"])
@@ -29,4 +51,168 @@ class HomePage(Page):
         FieldPanel("banner_subtitle"),
         FieldPanel("banner_image"),
         FieldPanel("banner_cta")
+    ]
+
+# ===================
+# Menu
+# ===================
+class MenuItem(Orderable):
+
+    link_title = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50
+    )
+    link_url = models.CharField(
+        max_length=500,
+        blank=True
+    )
+    link_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        related_name="+",
+        on_delete=models.CASCADE,
+    )
+    open_in_new_tab = models.BooleanField(default=False, blank=True)
+
+    page = ParentalKey("Menu", related_name="menu_items")
+
+    panels = [
+        FieldPanel("link_title"),
+        FieldPanel("link_url"),
+        PageChooserPanel("link_page"),
+        FieldPanel("open_in_new_tab"),
+    ]
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        elif self.link_url:
+            return self.link_url
+        return '#'
+
+    @property
+    def title(self):
+        if self.link_page and not self.link_title:
+            return self.link_page.title
+        elif self.link_title:
+            return self.link_title
+        return 'Missing Title'
+
+
+@register_snippet
+class Menu(ClusterableModel):
+    """The main menu clusterable model."""
+
+    title = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from="title", editable=True)
+    # slug = models.SlugField()
+
+    panels = [
+        MultiFieldPanel([
+            FieldPanel("title"),
+            FieldPanel("slug"),
+        ], heading="Menu"),
+        InlinePanel("menu_items", label="Menu Item")
+    ]
+
+    def __str__(self):
+        return self.title
+    
+
+# ===================
+# Contact From
+# ===================
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+class ContactPage(AbstractEmailForm):
+    templates = "templates/home/contact_page.html"
+    landing_page_templates = "templates/home/contact_page_landing.html"
+
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+    page_title = models.CharField(max_length=100, blank=False, null=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("page_title"),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label='Form Fields'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel("subject"),
+        ], heading="Email Settings"),
+    ]
+
+
+        
+# ===================
+# Blog
+# ===================
+class BlogListingPage(Page):
+    """Listing page lists all the Blog Detail Pages."""
+
+    templates = "templates/home/blog_listing_page.html"
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Overwrites the default title',
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        context["posts"] = BlogDetailPage.objects.live().public()
+        return context
+
+
+class BlogDetailPage(Page):
+    templates = "templates/home/blog_details_page.html"
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Overwrites the default title',
+    )
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=False,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
+    blog_details_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        FieldPanel("blog_image"),
+        FieldPanel("blog_details_cta"),
+        FieldPanel("content"),
     ]
